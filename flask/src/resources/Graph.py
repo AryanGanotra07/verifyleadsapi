83715f6b8a575a6d2b264ce136bd33df1a8d15e5
Aryan Ganotra <aryanganotra7@gmail.com>
2020-07-17 20:06:52 +0530

    tries

--- flask/src/resources/Graph.py
+++ flask/src/resources/Graph.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource
 from src.models.UserModel import UserModel
+from flask import request
 from flask_jwt_extended import get_jwt_claims, jwt_required
 
 class Graph(Resource):
@@ -9,4 +10,5 @@ class Graph(Resource):
         claims = get_jwt_claims()
         if not claims['isAdmin']:
              return {'message' : 'Admin priviledge required'} , 401
+        print(request.remote_addr)
         return UserModel.get_recent_accounts()
