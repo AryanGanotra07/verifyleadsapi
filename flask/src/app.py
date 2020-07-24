c43eee3c417248871be24b6c3c20c6ea3ae93d18
Aryan Ganotra <aryanganotra7@gmail.com>
2020-07-24 22:21:59 +0530

    some

--- flask/src/app.py
+++ flask/src/app.py
@@ -79,7 +79,7 @@ def create_app():
       if request.headers['Origin'] in white_origin:
         response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] 
         response.headers['Access-Control-Allow-Methods'] = 'PUT,GET,POST,DELETE'
-        response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
+        response.headers['Access-Control-Allow-Headers'] = 'Origin,Content-Type,Authorization'
       return response
 
   
